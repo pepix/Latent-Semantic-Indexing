a8374b25108a3f70487276b02e9db2d47ad9a312
pepix <pepix2112@gmail.com>
2018-01-01 17:20:45 +0900

    [fix]mylist

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Latent-Semantic-Indexing
 For the purpose of analyzing articles, twitter and novels.
+
+## Caution
+This code needs to see input documents, so current directory has data subfolder and move each documents to data folder.
--- lsi.py
+++ lsi.py
@@ -10,8 +10,7 @@ from sklearn.feature_extraction import DictVectorizer
 mcb = MeCab("-Owakati")
 v = DictVectorizer()
 mylist = []
-mini1 = []
-mini2 = []
+stopword = ["する", "です", "、", "。", "ます", "が", "を", "あり", "ない", "いる", "も", "さ", "ある", "こと", "れ", "で", "だ", "し", "で", "と", "て", "は", "た", "（", "）", "な", "い", "や", "だっ", "よう", "れる", "から", "など", "ため", "なる", "なっ", "という", "でき", "的", "この", "その", "だけ", "それ", "しかし", "へ", "しよ", "として", "「", "」", "これ", "まで"]
 
 for i in range(20):
     exec("tango" + str(i) + "= []")
@@ -27,64 +26,43 @@ for name in data_list:
     news = f.read()
     wakati = mcb.parse(news).split(" ")
     mydict = collections.Counter(wakati)
-    print(mydict)
+    #print(mydict)
     f.close()
     # 出現頻度が高すぎる単語、低すぎる単語を除去
     for key in list(mydict):
-        if 3 <= mydict[key] and mydict[key] <= 20:
-            #print("tango" + str(cnt) + ".append(\"" + str(key) + "\")")
-            exec("tango" + str(cnt) + ".append(\"" + str(key) + "\")")
-    print(cnt)
+        if 1 <= mydict[key] and mydict[key] <= 10:
+            # ストップワードリストに含まれている単語を除去
+            if not key in stopword:
+                for k in range(mydict[key]):
+                    exec("tango" + str(cnt) + ".append(\"" + str(key) + "\")")
     cnt += 1
 
 for j in range(20):
-    exec("print(tango" + str(j) + ")")
+    exec("mylist.append(tango" + str(j) + ")")
+    #exec("print(tango" + str(j) + ")")
 
-
-
-
-f = open('it_news1.txt')
-news2 = f.read()  # ファイル終端まで全て読んだデータを返す
-f.close()
-
-wakati2 = mcb.parse(news2).split(" ")
-mydict2 = collections.Counter(wakati2)
-
-# 出現頻度が高すぎる単語、低すぎる単語を除去
-for key in list(mydict2):
-    if mydict2[key] <= 3:
-        mydict2.pop(key)
-    elif mydict2[key] >= 25:
-        mydict2.pop(key)
-    else:
-        mini2.append(key)
-
-mylist.append(mini1)
-mylist.append(mini2)
 #print(mylist)
 
-
 # 行列化
-#arr = v.fit_transform(mylist).toarray()
-#print(arr)
-#print(v.get_feature_names())
+# arr = v.fit_transform(mylist).toarray()
+# print(arr)
+# print(v.get_feature_names())
 
 dictionary = corpora.Dictionary(mylist)
-dictionary.save_as_text('deerwester.dict')
+dictionary.save_as_text('output.dict')
 
 #print(dictionary.token2id)
 
 # コーパス生成・外部保存
 corpus = [dictionary.doc2bow(text) for text in mylist]
-corpora.MmCorpus.serialize('deerwester.mm', corpus)
+corpora.MmCorpus.serialize('output.mm', corpus)
 #print(corpus)
 
 # コーパス読み込み
-mm = gensim.corpora.MmCorpus('deerwester.mm')
+mm = gensim.corpora.MmCorpus('output.mm')
 #print(mm)
 
 lsi = gensim.models.lsimodel.LsiModel(corpus=mm, id2word=dictionary, num_topics=2)
 
-for i in range(5):
-    print(lsi.print_topics(i))
-
+for i in lsi.print_topics(2):
+    print(i)
